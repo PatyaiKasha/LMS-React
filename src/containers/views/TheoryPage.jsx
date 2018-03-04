f63c16c3464d6aa16eeb559887bc612ff8af8160
y_tychinskaya <juljul1@gmail.com>
2018-03-04 17:35:25 +0200

    thery header add

--- src/containers/views/TheoryPage.jsx
+++ src/containers/views/TheoryPage.jsx
@@ -1,10 +1,10 @@
-
 import React from 'react';
+import HeaderMain from "@/components/Common/HeaderThery";
 
 const TheoryPage = () => (
   <div>
     <h1>Theory</h1>
-    
+    <HeaderMain module="Теория" themNum="1" themTitle="ДОМ" />
   </div>
 );
 
