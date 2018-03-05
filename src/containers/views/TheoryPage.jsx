3d6aa17a6fdc0af8f1643ebdf9aa6a1902a2056e
Mihail Yanenko <yanenko.web.dev@gmail.com>
2018-03-05 18:27:38 +0200

    added right nav

--- src/containers/views/TheoryPage.jsx
+++ src/containers/views/TheoryPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HeaderMain from "@/components/Common/HeaderThery";
+import RightNav from "@/components/Common/RightNav";
 
 const TheoryPage = () => (
   <div>
     <h1>Theory</h1>
     <HeaderMain module="Теория" themNum="1" themTitle="ДОМ" />
+    <RightNav />
   </div>
 );
 
