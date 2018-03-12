817855073c71861e79083e2f1667c89b7d40bac0
Serhii Klymenko <klimenko81@gmail.com>
2018-03-12 22:34:39 +0200

    copy Julias part to homework

--- src/containers/views/HomeworkPage.jsx
+++ src/containers/views/HomeworkPage.jsx
@@ -1,9 +1,13 @@
 
 import React from 'react';
+import HeaderMain from "@/components/Common/HeaderHomework";
+import RightNav from "@/components/Common/RightNav";
 
 const HomeworkPage = () => (
   <div>
-    <h1>Homework</h1>
+    <h1>HomeworkPage</h1>
+    <HeaderMain module="Теория" themNum="1" themTitle="ДОМ" />
+    <RightNav />
   </div>
 );
 
