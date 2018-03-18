7c84897d260264d751b2c8108a39b32db8f757ac
Mihail Yanenko <yanenko.web.dev@gmail.com>
2018-03-18 10:46:26 +0200

    added tooltip

--- src/components/Common/RightNav/index.jsx
+++ src/components/Common/RightNav/index.jsx
@@ -1,30 +1,38 @@
 import React from 'react';
+import ReactTooltip from 'react-tooltip';
 import './style.scss';
 import './main.scss';
 
 export default class RightNav extends React.Component {
+
   render() {
         return (
             <div className="right-nav">
                 <div className="m-right-nav">
-                    <a href="#"
+                    <a href="#" data-tip="Добавить в избранное _"
                         className="m-right-nav__item m-right-nav__item-favorites">
                     </a>
-                    <a href="#"
+                        <ReactTooltip place="left" type="warning" effect="solid"/>
+                    <a href="#" data-tip="Напомнить позже _"
                         className="m-right-nav__item m-right-nav__item-reminder">
                     </a>
-                    <a href="#"
+                        <ReactTooltip place="left" type="warning" effect="solid"/>
+                    <a href="#" data-tip="Обсудить на форуме _"
                         className="m-right-nav__item m-right-nav__item-forum">
                     </a>
-                    <a href="#"
+                        <ReactTooltip place="left" type="warning" effect="solid"/>
+                    <a href="#" data-tip="Обсудить в чате _"
                         className="m-right-nav__item m-right-nav__item-chat">
-                    </a> 
-                    <a href="#"
+                    </a>
+                        <ReactTooltip place="left" type="warning" effect="solid"/>
+                    <a href="#" data-tip="Задать вопрос ментору _"
                         className="m-right-nav__item m-right-nav__item-mentor">
                     </a>
-                    <a href="#"
+                        <ReactTooltip place="left" type="warning" effect="solid"/>
+                    <a href="#" data-tip="Выделить текст _"
                         className="m-right-nav__item m-right-nav__item-select">
                     </a>
+                        <ReactTooltip place="left" type="warning" effect="solid"/>
                 </div>
             </div>
         );
