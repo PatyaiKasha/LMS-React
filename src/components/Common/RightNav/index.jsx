efad6c12d2723cc7ab6ee7829fc7a87efc6179fe
Mihail Yanenko <yanenko.web.dev@gmail.com>
2018-03-18 18:40:11 +0200

    change color tooltips

--- src/components/Common/RightNav/index.jsx
+++ src/components/Common/RightNav/index.jsx
@@ -12,27 +12,22 @@ export default class RightNav extends React.Component {
                     <a href="#" data-tip="Добавить в избранное _"
                         className="m-right-nav__item m-right-nav__item-favorites">
                     </a>
-                        <ReactTooltip place="left" type="warning" effect="solid"/>
                     <a href="#" data-tip="Напомнить позже _"
                         className="m-right-nav__item m-right-nav__item-reminder">
                     </a>
-                        <ReactTooltip place="left" type="warning" effect="solid"/>
                     <a href="#" data-tip="Обсудить на форуме _"
                         className="m-right-nav__item m-right-nav__item-forum">
                     </a>
-                        <ReactTooltip place="left" type="warning" effect="solid"/>
                     <a href="#" data-tip="Обсудить в чате _"
                         className="m-right-nav__item m-right-nav__item-chat">
                     </a>
-                        <ReactTooltip place="left" type="warning" effect="solid"/>
                     <a href="#" data-tip="Задать вопрос ментору _"
                         className="m-right-nav__item m-right-nav__item-mentor">
                     </a>
-                        <ReactTooltip place="left" type="warning" effect="solid"/>
                     <a href="#" data-tip="Выделить текст _"
                         className="m-right-nav__item m-right-nav__item-select">
                     </a>
-                        <ReactTooltip place="left" type="warning" effect="solid"/>
+                        <ReactTooltip class='extraClass' place="left" effect="solid"/>
                 </div>
             </div>
         );
