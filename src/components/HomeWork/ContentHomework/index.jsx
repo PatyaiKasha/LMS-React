ab13fe6fd01b59a8cf14d587f6a2494a1b329ab2
Serhii Klymenko <klimenko81@gmail.com>
2018-03-23 20:20:57 +0200

    copy Julias part to homework

--- src/components/HomeWork/ContentHomework/index.jsx
+++ src/components/HomeWork/ContentHomework/index.jsx
@@ -5,12 +5,11 @@ export default class  ContentMain extends React.Component {
     render() {
         const {textq} = this.props;
         return (
-          <div id="m-title" className="m-header">
-              <div className="m-header__caption caption">
-                  <h2 className="caption__title">{textq}</h2>
-            
-              </div>
+          <div id="ContentHomework">
               
+                  <p className="ContentHomework">{textq}</p>
+            
+ 
           </div>
       );
       }
