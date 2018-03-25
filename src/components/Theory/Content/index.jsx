34195736618150dd22c8c798683ada13c41910ec
y_tychinskaya <juljul1@gmail.com>
2018-03-25 20:08:59 +0300

    add map

--- src/components/Theory/Content/index.jsx
+++ src/components/Theory/Content/index.jsx
@@ -2,43 +2,16 @@ import React from 'react';
 
 import './styles.scss';
 
-export default class Content extends
-React.Component {
+const Content = ({contentTitle, contentText}) =>( 
+    <dl className="m-content">
+        <h4 className="m-content__content-title">[{ contentTitle}_]</h4>
+        <p className="m-content__text">{contentText}</p>
+        
+         {/* <a href="#m-title" className="m-btn-up invisible">
+                </a> */}
+    </dl>
 
-    render() {
-        const { content, text}= this.props;
-        return (
-            <dl className="m-content">
-                <h4 className="m-content__content-title">[{ content}_]</h4>
-                <p className="m-content__text">{text}</p>
-                {/* // <p className="m-content__text">text</p>
-                // <p className="m-content__text">text</p> */}
-                <h4 className="m-content__content-title">[ {content}_]</h4>
-                <p className="m-content__text">{text}</p>
-                {/* // <p className="m-content__text">text</p>
-                // <p className="m-content__text">text</p> */}
-                <h4 className="m-content__content-title">[ {content}_]</h4>
-                <p className="m-content__text">{text}</p>
-                {/* // <p className="m-content__text">text</p> */}
-                <h4 className="m-content__content-title">[ {content}_]</h4>
-                <p className="m-content__text">{text}</p>
-                <h4 className="m-content__content-title">[ {content}_]</h4>
-                <p className="m-content__text">{text}</p>
-                {/* // <p className="m-content__text">text</p>
-                // <p className="m-content__text">text</p> */}
-                <h4 className="m-content__content-title">[ {content}_]</h4>
-                <p className="m-content__text">{text}</p>
-                {/* // <p className="m-content__text">text</p> */}
-                <h4 className="m-content__content-title">[ {content}_]</h4>
-                <p className="m-content__text">{text}</p>
-                {/* // <p className="m-content__text">text</p> */}
-                <h4 className="m-content__content-title">[ {content}_]</h4>
-                <p className="m-content__text">{text}</p>
+    );
 
-                <a href="#m-title" className="m-btn-up invisible">
-                    
-                </a>
-            </dl>
-        )
-    }
-}
+
+export default Content;
