b7de58bd20e3c426c7519c59a9b98995df14d979
Serhii Klymenko <klimenko81@gmail.com>
2018-03-25 21:33:23 +0300

    new

--- src/components/HomeWork/ContentHomework/index.jsx
+++ src/components/HomeWork/ContentHomework/index.jsx
@@ -8,7 +8,28 @@ export default class  ContentMain extends React.Component {
           <div id="ContentHomework">
               
                   <p className="ContentHomework">{textq}</p>
-            
+                  <div>
+                            <form className="d-rating-form" action="#" method="post">
+                              
+                                <input type="text" className="d-rating__choose" name="choose" placeholder="Ссылка на GitHub"></input>
+                                <button className="d-rating__done" type="submit" name="d-sub">Готово</button>
+                            </form>
+                            <p className="d-rating__line">
+                            <span className="d-rating__digit">3/10</span>
+                             баллов</p>
+                        </div>
+                        <div classNme="d-comments">
+                            <form className="d-comments-form" action="#" method="post">
+                                
+                                <label className="d-comments__name"></label>
+                                <input className="d-comments__title" type="text" name="d-comments" value="" placeholder="Написать комментарий"></input>
+                                <button className="d-comments__send" type="submit" name="d-sub" value="sent">Отправить</button>
+                            </form>
+                        </div>
+                        <div className="d-nav">
+                            <a href="#" className="d-nav__left">Вернуться к тестам</a>
+                            <a href="#" className="d-nav__right">Следующая тема</a>
+                        </div>
  
           </div>
       );
