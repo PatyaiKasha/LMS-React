8fbd080594527a9ec127eb74d71668b6472c0877
Igor Protsyuk <batko_zeus@i.ua>
2018-04-24 22:59:36 +0300

    created timer for Task Page

--- src/components/Tests/pages/tasks.jsx
+++ src/components/Tests/pages/tasks.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import './styles.scss';
 import TestsHeader from '../TestsHeader';
 import TestsContentTasks from '../TestsContentTasks';
 import TestsFooter from '../TestsFooter';
-import TestsTimer from '../TestsTimer';
 import TestsEndTaskButton from '../TestsEndTaskButton';
 import testsData from '../testDatabase';
 
@@ -15,27 +15,91 @@ export default class TasksPageTests extends React.Component {
   };
 
   state = {
-    userTestAnswers: []
+    userTestAnswers: [],
+    timerMinutes: testsData.theme_time.theme_minutes,
+    timerSeconds: testsData.theme_time.theme_seconds,
+    endInterval: this.props.stopInterval
   };
 
+  componentDidMount() {
+    this.testsInterval = setInterval(this.tick, 100);
+  };
+
+  componentWillUnmount () {
+    clearInterval(this.testsInterval);
+  };
+
+  tick = () => {
+    if (this.state.endInterval) {
+      clearInterval(this.testsInterval);
+    }
+    else {
+      let minute = this.state.timerMinutes;
+      let second = this.state.timerSeconds;
+
+      if ( second > 0 ) {
+        second--;
+        let secondResult = (second < 10 ) ? `0${second}` : `${second}`;
+        this.setState({
+          timerSeconds: secondResult
+        });
+      }
+      else {
+        second = 59;
+        let secondResult = (second < 10 ) ? `0${second}` : `${second}`;
+        this.setState({
+          timerSeconds: secondResult
+        });
+
+        if ( minute > 0 ) {
+          minute--;
+          let minuteResult = (minute < 10 ) ? `0${minute}` : `${minute}`;;
+          this.setState({
+            timerMinutes: minuteResult
+          }); 
+        }
+        else {
+          this.setState({
+            timerSeconds: '00'
+          });
+          this.setState({
+            endInterval: true
+          });
+
+        }
+      }
+    } 
+  };
 
   onSubmitVariants = (tests) => {
     this.setState({
       userTestAnswers: tests
-    })
+    });
   };
 
+
   render() {
-    return (
-	    <div className='t-main-tests'>
-	    	<TestsHeader theme_number={testsData.theme_number} theme_title={testsData.theme_title} >
-          <TestsTimer />
-        </TestsHeader>
-        <TestsContentTasks question_quan={testsData.theme_tests.length} tests_list={testsData.theme_tests} selected_variants={this.onSubmitVariants} />
-        <TestsFooter>
-          <TestsEndTaskButton onClick={() => {this.props.sended_variants(this.state.userTestAnswers);}} />
-        </TestsFooter>
-	    </div>
-    );
+    const { userTestAnswers, timerMinutes, timerSeconds, endInterval } = this.state;
+
+    if (endInterval === true) {
+      return (
+        <Redirect to="/tests/timeout" />
+      );
+    }
+    else {
+      return (
+        <div className='t-main-tests'>
+          <TestsHeader theme_number={testsData.theme_number} theme_title={testsData.theme_title} >
+            <span className="t-main-span">
+              <span className="t-timer__minutes">{timerMinutes}</span>: <span className="t-timer__seconds">{timerSeconds}</span>
+            </span>
+          </TestsHeader>
+          <TestsContentTasks question_quan={testsData.theme_tests.length} tests_list={testsData.theme_tests} selected_variants={this.onSubmitVariants} />
+          <TestsFooter>
+            <TestsEndTaskButton onClick={() => {this.props.sended_variants(userTestAnswers, timerSeconds, timerMinutes);}} />
+          </TestsFooter>
+        </div>
+      );
+    }  
   }
 }
