968d95706a58d31b7f13ed84f42d1d88b1b7a2c6
Igor Protsyuk <batko_zeus@i.ua>
2018-04-24 22:31:18 +0300

    Changed main

--- src/components/Tests/index.jsx
+++ src/components/Tests/index.jsx
@@ -1,27 +1,48 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import MainPage from './pages/main';
-import TasksPage from './pages/tasks';
-import ResultsPage from './pages/results';
+import TasksPage from './pages/tasks2';
+import ResultsPage from './pages/results2';
+import TimeoutPage from './pages/timerout';
 
 
 export default class Tests extends React.Component {
 	state = {
-	    readyUserTestAnswers: []
+	    readyUserTestAnswers: [],
+	    stopInterval: false,
+	    showSeconds: '',
+	    showMinutes: ''
 	};
 
-	prepareToChecking = (prepared) => {
+	prepareToChecking = (prepared, seconds, minutes) => {
 		this.setState({
 	    	readyUserTestAnswers: prepared
-	    }) 
+	    });
+	    this.setState({
+	    	stopInterval: true
+	    });
+	    this.setState({
+	    	showSeconds: seconds
+	    });
+	    this.setState({
+	    	showMinutes: minutes
+	    });
 	};
+
+	resetTimer = () => {
+		this.setState({
+	    	stopInterval: false
+	    });
+	};
+
 
   	render() {
 	    return (
 	    	<Switch>
 		        <Route exact path={`${this.props.match.path}/`} render={() => <MainPage />} />
-		        <Route path={`${this.props.match.path}/tasks`} render={() => <TasksPage sended_variants={this.prepareToChecking} />} />
-		        <Route path={`${this.props.match.path}/results`} render={() => <ResultsPage prepared_variants={this.state.readyUserTestAnswers} />} />
+		        <Route path={`${this.props.match.path}/tasks`} render={() => <TasksPage sended_variants={this.prepareToChecking} stopInterval={this.state.stopInterval} />} />
+		        <Route path={`${this.props.match.path}/results`} render={() => <ResultsPage prepared_variants={this.state.readyUserTestAnswers} stopInterval={this.state.stopInterval} resetTimer={this.resetTimer} showSeconds={this.state.showSeconds} showMinutes={this.state.showMinutes} />} />
+		        <Route path={`${this.props.match.path}/timeout`} render={() => <TimeoutPage />} />
 	    	</Switch>
 	    );
   	}
